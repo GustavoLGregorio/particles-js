ebcd1a37f6cd294dc167b404655bd91c48e91d79
Gustavo Luiz Gregorio <gustavo.l.gregorio@gmail.com>
2025-10-01 07:39:49 -0300

    Updated lib config

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-import ParticlesJS from "./particles";
+import ParticlesJS from "./particles.js";
 
 const P = new ParticlesJS();
 
 P.config = {
 	canvas: {
 		appendCanvasTo: document.body,
-		backgroundColor: "#000",
-		size: { width: window.innerWidth, height: window.innerHeight },
-		smoothing: undefined,
+		backgroundColor: "#101010",
+		size: { width: 350, height: 250 },
 		threshold: window.innerWidth * 1.3,
 	},
 	particles: {
-		quantity: 2_000,
+		quantity: 1_000,
 		maxLength: 5,
-		maxSize: 5,
-		velocity: 5,
-		lifespan: 3 * 60,
-		spreadFactor: 3,
-		color: null,
+		maxSize: 10,
+		velocity: 0,
+		lifespan: 2 * 60,
+		spreadFactor: 2,
+		color: "white",
 	},
+	targets: [],
+	spawners: [],
 	listeners: {
 		spawners: {
 			enableListener: true,
 			keyboardTrigger: "Shift",
+			identifier: {
+				color: "red",
+				size: 12,
+				font: "monospace",
+			},
 		},
 		targets: {
 			enableListener: true,
--- particles.js
+++ particles.js
@@ -1,35 +1,43 @@
 /** LIB TYPES */
 /**
  * @typedef ScreenDimensions
- * @property {number} [width]
- * @property {number} [height]
+ * @property {number} width
+ * @property {number} height
  */
 
 /**
  * @typedef CanvasConfig
- * @property {ScreenDimensions} [size]
- * @property {number} [threshold]
- * @property {string} [backgroundColor]
- * @property {HTMLElement} [appendCanvasTo]
+ * @property {ScreenDimensions} size
+ * @property {number} threshold
+ * @property {string} backgroundColor
+ * @property {HTMLElement} appendCanvasTo
  * @property {"low" | "medium" | "high"} [smoothing]
  */
 
 /**
  * @typedef ParticleConfig
- * @property {number} [velocity]
- * @property {number} [quantity]
- * @property {number} [maxLength]
+ * @property {number} velocity
+ * @property {number} quantity
+ * @property {number} maxLength
  * @property {number} [maxSize]
  * @property {string | null} [color]
  * @property {number} [lifespan]
  * @property {Partial<Vec2>} [initialTargetPosition]
- * @property {number} [spreadFactor]
+ * @property {number} spreadFactor
+ */
+
+/**
+ * @typedef ListenerIdentifier
+ * @property {string} color
+ * @property {number} size
+ * @property {string} font
  */
 
 /**
  * @typedef Listener
  * @property {boolean} enableListener
  * @property {string} keyboardTrigger
+ * @property {ListenerIdentifier} [identifier]
  */
 
 /**
@@ -39,9 +47,11 @@
  */
 
 /**
- * @typedef Configs
+ * @typedef ParticlesJSConfig
  * @property {CanvasConfig} canvas
  * @property {ParticleConfig} particles
+ * @property {Vec2[]} targets
+ * @property {Vec2[]} spawners
  * @property {ListenersConfig} [listeners]
  */
 
@@ -53,7 +63,7 @@
  */
 
 class ParticlesJS {
-	/** @type {Configs | null} */
+	/** @type {ParticlesJSConfig | null} */
 	#config = null;
 
 	/** @type {HTMLCanvasElement | null} */
@@ -79,29 +89,59 @@ class ParticlesJS {
 
 	// --- GETTERS & SETTERS ---
 
-	/** @param {Configs} configValue */
+	/** @param {ParticlesJSConfig} configValue */
 	set config(configValue) {
 		this.#config = configValue;
+		this.#init();
 	}
 
-	/** @returns {Configs | null} */
+	/** @returns {ParticlesJSConfig | null} */
 	get config() {
 		return this.#config;
 	}
 
 	// --- METHODS ---
 	// INITIALIZE OBJECT AND CANVAS CONFIG
-	#init() {}
+	#init() {
+		if (!this.#config) return;
+
+		// CANVAS CONFIG
+		this.#canvas = document.createElement("canvas");
+		this.#config.canvas.appendCanvasTo.append(this.#canvas);
+
+		this.#canvas.width = this.#config.canvas.size.width;
+		this.#canvas.height = this.#config.canvas.size.height;
+		this.#canvas.style.backgroundColor = this.#config.canvas.backgroundColor;
+
+		this.#ctx = this.#canvas.getContext("2d");
+
+		// image smoothing logic
+		if (this.#ctx && this.#config.canvas.smoothing) {
+			this.#ctx?.imageSmoothingEnabled;
+			this.#ctx.imageSmoothingQuality = this.#config.canvas.smoothing;
+		}
+
+		this.#render();
+	}
 
 	// PARTICLE CREATION
 	/** @returns {Particle} */
 	#getParticle() {
-		return new Particle({}, {}, [], []);
+		if (!this.#config) {
+			throw new Error("Object was not configured");
+		}
+
+		const targets = this.#config.targets ? this.#config.targets : [];
+		const spawners = this.#config.spawners ? this.#config.spawners : [];
+
+		return new Particle(this.#config.particles, this.#config.canvas, spawners, targets);
 	}
 
 	// RENDERING LOGIC
 	#render() {
-		if (this.#ctx === undefined) return;
+		if (!this.#config || !this.#ctx) return;
+
+		const config = this.#config;
 
 		/** @type {Particle[]} */
 		const ps = [];
@@ -111,13 +151,13 @@ class ParticlesJS {
 			y = 0;
 
 		const loop = () => {
-			if (!this.#ctx) return;
+			if (!this.#config || !this.#ctx) return;
 
-			this.#ctx.fillStyle = CANVAS_BACKGROUND_COLOR;
-			this.#ctx.fillRect(0, 0, CANVAS_SIZE_X, CANVAS_SIZE_Y);
+			this.#ctx.fillStyle = config.canvas.backgroundColor;
+			this.#ctx.fillRect(0, 0, config.canvas.size.width, config.canvas.size.height);
 
-			if (ps.length < PARTICLE_NUMBER) {
-				for (; i < PARTICLE_NUMBER - ps.length; ++i) ps.push(this.#getParticle());
+			if (ps.length < config.particles.quantity) {
+				for (; i < config.particles.quantity - ps.length; ++i) ps.push(this.#getParticle());
 				i = 0;
 			}
 
@@ -140,31 +180,34 @@ class ParticlesJS {
 
 				if (
 					ps[i].lifespan <= 0 ||
-					ps[i].pos.x < -PARTICLE_MAX_LENGTH * 2 ||
-					ps[i].pos.x > CANVAS_SIZE_X + PARTICLE_MAX_LENGTH * 2 ||
-					ps[i].pos.y < -PARTICLE_MAX_LENGTH * 2 ||
-					ps[i].pos.y > CANVAS_SIZE_Y + (CANVAS_THRESHOLD + PARTICLE_MAX_LENGTH)
+					ps[i].pos.x < -config.particles.maxLength * 2 ||
+					ps[i].pos.x > config.canvas.size.width + config.particles.maxLength * 2 ||
+					ps[i].pos.y < -config.particles.maxLength * 2 ||
+					ps[i].pos.y >
+						config.canvas.size.height + (config.canvas.threshold + config.particles.maxLength)
 				) {
 					ps.splice(i, 1);
 				}
 			}
 			i = 0;
 
+			if (!config.targets) return;
 			// rendering targets
-			for (; i < targets.length; ++i) {
+			for (; i < config.targets.length; ++i) {
 				this.#ctx.fillStyle = "#ffffff95";
-				this.#ctx.fillRect(targets[i].x, targets[i].y, 4, 4);
+				this.#ctx.fillRect(config.targets[i].x, config.targets[i].y, 4, 4);
 				this.#ctx.fillStyle = "#fff";
-				this.#ctx.fillText(`${i}`, targets[i].x + 2, targets[i].y - 6, 12);
+				this.#ctx.fillText(`${i}`, config.targets[i].x + 2, config.targets[i].y - 6, 12);
 			}
 			i = 0;
 
+			if (!config.spawners) return;
 			// rendering spawnpoints
-			for (; i < spawners.length; ++i) {
+			for (; i < config.spawners.length; ++i) {
 				this.#ctx.fillStyle = "#4800c695";
-				this.#ctx.fillRect(spawners[i].x, spawners[i].y, 4, 4);
+				this.#ctx.fillRect(config.spawners[i].x, config.spawners[i].y, 4, 4);
 				this.#ctx.fillStyle = "#0fde00ff";
-				this.#ctx.fillText(`${i}`, spawners[i].x + 2, spawners[i].y - 6, 12);
+				this.#ctx.fillText(`${i}`, config.spawners[i].x + 2, config.spawners[i].y - 6, 12);
 			}
 			i = 0;
 
@@ -195,133 +238,136 @@ class ParticlesJS {
 }
 
 /** GLOBAL CONSTANTS */
-// window
-const WINDOW_W = window.innerWidth,
-	WINDOW_H = window.innerHeight;
+// // window
+// const WINDOW_W = window.innerWidth,
+// 	WINDOW_H = window.innerHeight;
 
-// canvas
-const CANVAS_SIZE_X = 400,
-	CANVAS_SIZE_Y = 400,
-	CANVAS_THRESHOLD = CANVAS_SIZE_X * 1.3,
-	CANVAS_BACKGROUND_COLOR = "#000";
+// // canvas
+// const CANVAS_SIZE_X = 400,
+// 	CANVAS_SIZE_Y = 400,
+// 	CANVAS_THRESHOLD = CANVAS_SIZE_X * 1.3,
+// 	CANVAS_BACKGROUND_COLOR = "#000";
 
 // particles
-const PARTICLE_VELOCITY = 2,
-	PARTICLE_NUMBER = 5_000,
-	PARTICLE_MAX_LENGTH = 5,
-	PARTICLE_MAX_SIZE = 10,
-	PARTICLE_COLOR = null, //"#FF0",
-	PARTICLE_LIFESPAN = 4 * 60,
-	PARTICLE_TARGET_POSITION_X = -PARTICLE_MAX_LENGTH * 2,
-	PARTICLE_TARGET_POSITION_Y = CANVAS_SIZE_Y - PARTICLE_MAX_LENGTH * -2,
-	PARTICLE_SPREAD = 3;
+// const PARTICLE_VELOCITY = 2,
+// 	PARTICLE_NUMBER = 5_000,
+// 	PARTICLE_MAX_LENGTH = 5,
+// 	PARTICLE_MAX_SIZE = 10,
+// 	PARTICLE_COLOR = null, //"#FF0",
+// 	PARTICLE_LIFESPAN = 4 * 60,
+// 	PARTICLE_TARGET_POSITION_X = -PARTICLE_MAX_LENGTH * 2,
+// 	PARTICLE_TARGET_POSITION_Y = CANVAS_SIZE_Y - PARTICLE_MAX_LENGTH * -2,
+// 	PARTICLE_SPREAD = 3;
 
 /** CANVAS CONFIG */
-const canvas = document.createElement("canvas");
-document.body.append(canvas);
+// const canvas = document.createElement("canvas");
+// document.body.append(canvas);
 
-canvas.width = CANVAS_SIZE_X;
-canvas.height = CANVAS_SIZE_Y;
-canvas.style.backgroundColor = CANVAS_BACKGROUND_COLOR;
-const ctx = canvas.getContext("2d");
+// canvas.width = CANVAS_SIZE_X;
+// canvas.height = CANVAS_SIZE_Y;
+// canvas.style.backgroundColor = CANVAS_BACKGROUND_COLOR;
+// const ctx = canvas.getContext("2d");
 
 /** DRAWING POINTS (TARGETS) */
-/** @type {Vec2[]} */
-const targets = [];
-let targetsColor = "#FFF";
+// /** @type {Vec2[]} */
+// const targets = [];
+// let targetsColor = "#FFF";
 
-/** SPAWNER POINTS (PARTICLES STARTING COORDINATES) */
-/** @type {Vec2[]} */
-const spawners = [];
-let spawnersColor = "#FF0";
+// /** SPAWNER POINTS (PARTICLES STARTING COORDINATES) */
+// /** @type {Vec2[]} */
+// const spawners = [];
+// let spawnersColor = "#FF0";
 
-const cachedSpawners = localStorage.getItem("spawners");
-const cachedTargets = localStorage.getItem("targets");
+// const cachedSpawners = localStorage.getItem("spawners");
+// const cachedTargets = localStorage.getItem("targets");
 
-if (cachedSpawners) {
-	for (const spawner of JSON.parse(cachedSpawners)) {
-		spawners.push(spawner);
-	}
-}
-if (cachedTargets) {
-	for (const target of JSON.parse(cachedTargets)) {
-		targets.push(target);
-	}
-}
+// if (cachedSpawners) {
+// 	for (const spawner of JSON.parse(cachedSpawners)) {
+// 		spawners.push(spawner);
+// 	}
+// }
+// if (cachedTargets) {
+// 	for (const target of JSON.parse(cachedTargets)) {
+// 		targets.push(target);
+// 	}
+// }
 
 /** LISTENERS AND ADDING POINTS LOGIC */
-/** @type {number[]} */
-const keysPressed = [];
-window.addEventListener("keydown", (key) => {
-	if (key.key === "Shift") keysPressed.push(0);
-	else if (key.key === "Control") keysPressed.push(1);
-	else if (key.key === "r") {
-		localStorage.clear();
-		window.location.reload();
-	} else if (key.key === "d") {
-		const link = document.createElement("a");
-		const objData = `{"spawners": ${JSON.stringify(targets)}, "targets": ${JSON.stringify(
-			spawners,
-		)}}`;
-		const blobData = new Blob([objData], {
-			type: "application/json",
-		});
-		const objURL = URL.createObjectURL(blobData);
-		link.href = objURL;
-		link.download = "blob1.json";
-		link.click();
-	}
-});
-window.addEventListener("keyup", () => {
-	keysPressed.splice(0, 2);
-});
-canvas.addEventListener("click", (mouse) => {
-	if (keysPressed.includes(0)) {
-		targets.push({ x: mouse.offsetX, y: mouse.offsetY });
-		localStorage.setItem("targets", JSON.stringify(targets));
-	} else if (keysPressed.includes(1)) {
-		spawners.push({ x: mouse.offsetX, y: mouse.offsetY });
-		localStorage.setItem("spawners", JSON.stringify(spawners));
-	}
-});
+// /** @type {number[]} */
+// const keysPressed = [];
+// window.addEventListener("keydown", (key) => {
+// 	if (key.key === "Shift") keysPressed.push(0);
+// 	else if (key.key === "Control") keysPressed.push(1);
+// 	else if (key.key === "r") {
+// 		localStorage.clear();
+// 		window.location.reload();
+// 	} else if (key.key === "d") {
+// 		const link = document.createElement("a");
+// 		const objData = `{"spawners": ${JSON.stringify(targets)}, "targets": ${JSON.stringify(
+// 			spawners,
+// 		)}}`;
+// 		const blobData = new Blob([objData], {
+// 			type: "application/json",
+// 		});
+// 		const objURL = URL.createObjectURL(blobData);
+// 		link.href = objURL;
+// 		link.download = "blob1.json";
+// 		link.click();
+// 	}
+// });
+// window.addEventListener("keyup", () => {
+// 	keysPressed.splice(0, 2);
+// });
+// canvas.addEventListener("click", (mouse) => {
+// 	if (keysPressed.includes(0)) {
+// 		targets.push({ x: mouse.offsetX, y: mouse.offsetY });
+// 		localStorage.setItem("targets", JSON.stringify(targets));
+// 	} else if (keysPressed.includes(1)) {
+// 		spawners.push({ x: mouse.offsetX, y: mouse.offsetY });
+// 		localStorage.setItem("spawners", JSON.stringify(spawners));
+// 	}
+// });
 
 /** PARTICLE */
 class Particle {
-	/** @type {Vec2} */
-	pos = { x: 0, y: 0 };
-
-	/** @type {number} */
-	size = Math.round(Math.random() * PARTICLE_MAX_SIZE);
-
-	/** @type {string} */
-	color;
-
-	/** @type {number} */
-	lifespan = Math.floor(Math.random() * PARTICLE_LIFESPAN);
-
-	/** @type {Vec2[]} */
-	trail = [];
-
-	/** @type {number} */
-	trailLength = Math.floor(Math.random() * PARTICLE_MAX_LENGTH);
-
-	/** @type {number} */
-	targetIndex = Math.floor(Math.random() * targets.length);
-
-	/** @type {Vec2} */
-	target =
-		targets.length > 0
-			? targets[this.targetIndex]
-			: { x: Math.random() * -CANVAS_SIZE_X, y: Math.random() * CANVAS_SIZE_Y * 2 };
-
-	/** @type {number} */
-	spawnIndex = Math.floor(Math.random() * spawners.length);
-
 	/** @type {ParticleConfig} */
 	config;
 
 	/** @type {CanvasConfig} */
 	canvasConfig;
+
+	/** @type {Vec2} */
+	pos = { x: 0, y: 0 };
+
+	/** @type {number} */
+	size;
+
+	/** @type {string} */
+	color;
+
+	/** @type {number} */
+	lifespan;
+
+	/** @type {Vec2[]} */
+	trail = [];
+
+	/** @type {number} */
+	trailLength;
+
+	/** @type {Vec2[]} */
+	targetsArr;
+
+	/** @type {Vec2[]} */
+	spawnersArr;
+
+	/** @type {number} */
+	targetIndex;
+
+	/** @type {Vec2} */
+	target;
+
+	/** @type {number} */
+	spawnIndex;
 
 	/**
 	 * @param {ParticleConfig} particleConfig
@@ -332,6 +378,8 @@ class Particle {
 	constructor(particleConfig, canvasConfig, spawners, targets) {
 		this.config = particleConfig;
 		this.canvasConfig = canvasConfig;
+		this.spawnersArr = spawners;
+		this.targetsArr = targets;
 
 		const PARTICLE_MAX_SIZE = particleConfig.maxSize || 10;
 		const PARTICLE_LIFESPAN = particleConfig.lifespan || 240;
@@ -355,14 +403,14 @@ class Particle {
 			spawners.length > 0
 				? spawners[this.spawnIndex].y
 				: Math.floor(Math.random() * CANVAS_SIZE_Y) - 50;
-		// prettier-ignore
 
 		// particle target
+		// prettier-ignore
 		this.target =
 			targets.length > 0 ? targets[this.targetIndex] :
 				{
-					x: particleConfig.initialTargetPosition?.x ?? -PARTICLE_MAX_LENGTH * 2,
-					y: particleConfig.initialTargetPosition?.y ?? CANVAS_SIZE_Y - PARTICLE_MAX_LENGTH * -2,
+					x: -PARTICLE_MAX_LENGTH * 2,
+					y: CANVAS_SIZE_Y - PARTICLE_MAX_LENGTH * -2,
 				};
 
 		// particle color
@@ -379,16 +427,16 @@ class Particle {
 	spread() {
 		switch (Math.round(Math.random() * 4)) {
 			case 0:
-				this.pos.x += PARTICLE_SPREAD;
+				this.pos.x += this.config.spreadFactor;
 				break;
 			case 1:
-				this.pos.x -= PARTICLE_SPREAD;
+				this.pos.x -= this.config.spreadFactor;
 				break;
 			case 2:
-				this.pos.y += PARTICLE_SPREAD;
+				this.pos.y += this.config.spreadFactor;
 				break;
 			case 3:
-				this.pos.y -= PARTICLE_SPREAD;
+				this.pos.y -= this.config.spreadFactor;
 				break;
 		}
 	}
@@ -408,9 +456,9 @@ class Particle {
 		const curveAmplitude = 5;
 		const curveFrequency = 0.1;
 
-		if (distance > PARTICLE_VELOCITY) {
-			const vx = (dx / distance) * PARTICLE_VELOCITY;
-			const vy = (dy / distance) * PARTICLE_VELOCITY;
+		if (distance > this.config.velocity) {
+			const vx = (dx / distance) * this.config.velocity;
+			const vy = (dy / distance) * this.config.velocity;
 
 			const perpX = -vy;
 			const perpY = vx;
@@ -422,19 +470,14 @@ class Particle {
 		}
 
 		if (
-			Math.abs(this.pos.x - target.x) < PARTICLE_VELOCITY &&
-			Math.abs(this.pos.y - target.y) < PARTICLE_VELOCITY
+			Math.abs(this.pos.x - target.x) < this.config.velocity &&
+			Math.abs(this.pos.y - target.y) < this.config.velocity
 		) {
-			if (targets.length > 0) {
-				this.target = targets[0];
+			if (this.targetsArr.length > 0) {
+				this.target = this.targetsArr[0];
 			}
 		}
 	}
 }
-
-// if (ctx) {
-// 	ctx.imageSmoothingEnabled = false;
-// 	render(ctx);
-// }
 
 export default ParticlesJS;
